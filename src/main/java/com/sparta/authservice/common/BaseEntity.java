db18abe742cc7a47ab05a9bb6d82f044626546de
goodperiodt <ilypsj@gmail.com>
2025-06-11 16:56:45 +0900

    feat: feat: JPA Auditing 설정 추가

--- src/main/java/com/sparta/authservice/common/BaseEntity.java
+++ src/main/java/com/sparta/authservice/common/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.sparta.authservice.common;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,11 +10,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // protected BaseEntity() {}
 public abstract class BaseEntity {
 
