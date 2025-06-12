ba76af881474c850e5acbb261e25b78921979ab6
goodperiodt <ilypsj@gmail.com>
2025-06-12 17:46:52 +0900

    test: LoginRequest - test 에서 객체 생성 목적으로 @NoArgsConstructor, @AllArgsConstructor 어노테이션 추가

--- src/main/java/com/sparta/authservice/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/sparta/authservice/presentation/dto/request/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.sparta.authservice.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "이메일은 필수 값입니다.")
     private String email;
