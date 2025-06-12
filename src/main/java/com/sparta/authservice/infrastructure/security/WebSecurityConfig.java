1cf9270d58e01a100dd70fa2c01fddb9c2b756f1
goodperiodt <ilypsj@gmail.com>
2025-06-12 14:25:27 +0900

    refactor: 클래스명 변경으로 인한 dependency 타입 변경(JwtTokenProvider -> JwtUtil)

--- src/main/java/com/sparta/authservice/infrastructure/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/authservice/infrastructure/security/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
