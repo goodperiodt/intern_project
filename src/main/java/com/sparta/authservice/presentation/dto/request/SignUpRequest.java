cfbd9ae584ab6492e6377510dde89b3b0489659e
goodperiodt <ilypsj@gmail.com>
2025-06-12 17:23:37 +0900

    test: 테스트에서 객체 생성 목적으로 SignUpRequest 클래스 레벨에 @NoArgsConstructor, @AllArgsConstructor 추가 작성

--- src/main/java/com/sparta/authservice/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/sparta/authservice/presentation/dto/request/SignUpRequest.java
@@ -2,9 +2,13 @@ package com.sparta.authservice.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest {
     @NotBlank(message = "이메일은 필수 값입니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
